7989679d32d1b359de146b1cbacf890375c6f3ad
TheKingsident <kingsident@gmail.com>
2024-04-19 05:24:38 +0100

    Added files for task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+"""
+Module of a type-annotated function floor
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Function floor which takes a float n as argument
+    """
+    return math.floor(n)
