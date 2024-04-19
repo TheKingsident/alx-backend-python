35a69bd33543d881d24cc1c3931dadcff6970af3
TheKingsident <kingsident@gmail.com>
2024-04-19 05:15:01 +0100

    Added files fpr task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module of a type-annotated function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function concat that takes a string str1 and a string str2 as arguments
+    """
+    return str1 + str2
