46b3f5c709f74bb65b4f39acd0b8c7cab9445965
TheKingsident <kingsident@gmail.com>
2024-04-19 05:00:07 +0100

    Added files for task 0

---
+++ 0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module of a type-annotated function add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function add that takes a float a and a float b as argumentSS
+    """
+    return a + b
