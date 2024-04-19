31e1afe114d57bfe00943d81492931d0b99c6f12
TheKingsident <kingsident@gmail.com>
2024-04-19 05:32:47 +0100

    Added files for task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module of a type-annotated function to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function to_str that takes a float n as argument
+    """
+    return str(n)
