cf1bb85e819029d33ed67c65cf5afca636e6696d
TheKingsident <kingsident@gmail.com>
2024-04-22 14:13:59 +0100

    Added files for task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module on an asynchronous coroutine
+"""
+
+import asyncio
+import random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay:int) -> list[float]:
+    """
+    Async routine called wait_n that takes in 2 int arguments
+    """
+
+    wait_times  =[]
+    delays = []
+    for _ in range(n):
+        wait_times.append(wait_random(max_delay))
+    
+    for wait_time in wait_times:
+        delay = await wait_time
+        delays.append(delay)
+    return delays
+
