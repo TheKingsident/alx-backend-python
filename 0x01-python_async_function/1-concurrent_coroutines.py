36b6b2de459235e5b2f298edbde2794ddebf019d
TheKingsident <kingsident@gmail.com>
2024-04-22 21:20:22 +0100

    Updated

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
-"""
-Module of an async coroutine
-"""
+""" Module of an async coroutine """
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-    """
-    Async routine called wait_n that takes in
-    2 int arguments
-    """
+    """ Async routine called wait_n that takes in 2 int arguments """
     wait_times = []
     delays = []
     for _ in range(n):
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-'''
-Module for measurimg time for execution
-'''
+''' Module for measurimg time for execution '''
 
 import time
 import asyncio
@@ -9,10 +7,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    '''
-    Measure_time function with integers n and max_delay
-    as arguments
-    '''
+    ''' Measure_time function with integers n and max_delay as arguments '''
     startTime = time.time()
     asyncio.run(wait_n(n, max_delay))
     timeUsed = time.time() - startTime
