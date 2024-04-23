cdc1b64eb580e24eafabb6e52e3700e582603e9b
TheKingsident <kingsident@gmail.com>
2024-04-23 20:47:25 +0100

    Updated some logic and added new files

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,11 @@
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """ The coroutine called async_generator that takes no arguments """
     for _ in range(10):
-        yield random.uniform(1, 10)
+        yield random.uniform(0, 10)
         await asyncio.sleep(1)
