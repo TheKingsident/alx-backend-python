8f95ceca010e502680af9ec77edabdacfa20ccee
TheKingsident <kingsident@gmail.com>
2024-04-26 08:32:43 +0100

    Updated

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:
     """ The coroutine called async_generator that takes no arguments """
     for _ in range(10):
         yield random.uniform(0, 10)
