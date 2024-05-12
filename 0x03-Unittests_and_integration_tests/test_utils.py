588d6f18f6ab94ce1d8e177b6b6b65bec3d2a5bd
TheKingsident <kingsident@gmail.com>
2024-05-12 20:47:47 +0100

    Updated for task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,13 +3,16 @@
 """
 
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch, MagicMock
 from parameterized import parameterized
+import requests
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """TestAccessNestedMap class
     """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -29,3 +32,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ TestGetJson class
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ method to test that utils.get_json returns the expected result
+        """
+        expected_json = test_payload
+        mock_response = MagicMock()
+        mock_response.json.return_value = expected_json
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        self.assertEqual(result, expected_json)
+        mock_get.assert_called_once_with(test_url)
