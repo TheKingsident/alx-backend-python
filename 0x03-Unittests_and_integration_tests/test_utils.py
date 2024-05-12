b4f22d953348cbe9c608aa1e65f00cee29b2685f
TheKingsident <kingsident@gmail.com>
2024-05-12 20:07:06 +0100

    Updated for task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" 0. Parameterize a unit test
+""" 0 - 1. Parameterize a unit test
 """
 
 import unittest
@@ -19,3 +19,13 @@ class TestAccessNestedMap(unittest.TestCase):
         """ method to test that the method returns what it is supposed to
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ Uses the assertRaises context manager to test for KeyError(s)
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
