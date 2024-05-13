2e5ab620c6b40b11eab47fa816f4806e41a426ca
TheKingsident <kingsident@gmail.com>
2024-05-13 21:43:53 +0100

    Updated for task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -37,7 +37,8 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, expected_result)
 
     def test_public_repos_url(self):
-        """ public repos url unittesting """
+        """ Test _public_repos_url method
+        """
 
         with patch('client.GithubOrgClient.org',
                    new_callable=PropertyMock,
@@ -47,3 +48,28 @@ class TestGithubOrgClient(unittest.TestCase):
             client = GithubOrgClient("Google")
             result = client._public_repos_url
             self.assertEqual(result, patched.return_value['repos_url'])
+
+    @patch('client.get_json')
+    @patch.object(GithubOrgClient, '_public_repos_url',
+                  new_callable=PropertyMock)
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        """ Test _public_repos method
+        """
+        url = 'https://api.github.com/orgs/google/repos'
+        mock_public_repos_url.return_value = url
+        # Mock the get_json function
+        mock_get_json.return_value = [
+            {'name': 'repo1', 'license': {'key': 'MIT'}},
+            {'name': 'repo2', 'license': None},
+            {'name': 'repo3', 'license': {'key': 'GPL'}}
+        ]
+
+        client = GithubOrgClient("Google")
+
+        result = client.public_repos(license='MIT')
+
+        self.assertEqual(result, ['repo1'])
+
+        mock_public_repos_url.assert_called_once()
+
+        mock_get_json.assert_called_once_with(url)
