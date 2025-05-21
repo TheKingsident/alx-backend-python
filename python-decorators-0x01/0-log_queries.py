8456b8a3ce9323e7cc7388a7d732b29c40f3ef92
TheKingsident <kingsident@gmail.com>
2025-05-21 12:30:18 +0100

    Fix typo in log_queries decorator comment and enhance query logging with timestamp

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,11 +1,12 @@
 import sqlite3
+from datetime import datetime
 import functools
 
-#### decorator to lof SQL queries
+#### decorator to log SQL queries
 
 def log_queries(func):
     def wrapper_log_queries(*args, **kwargs):
-        print(f'Executing query: {args[0]}')
+        print(f'Executing query: {args[0]} on {datetime.now()}')
         return func(*args, **kwargs)
     return wrapper_log_queries
 
