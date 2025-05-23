3099551ff2fb68fb27a0116a570f6ddc7f92212e
TheKingsident <kingsident@gmail.com>
2025-05-23 20:32:00 +0100

    Add docstring to DatabaseConnection class for context manager implementation

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,6 @@
+""""
+In creating the DatabaseConnection class-based context manager, we will:
+1. define the init method which will create and establish teh connection to the database.
+2. define the enter method which will execute and return the result of the query on the database
+3. define the exit method which will close connection to the database.
+"""
