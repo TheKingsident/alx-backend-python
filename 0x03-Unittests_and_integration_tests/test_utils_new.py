7232536aa0022447950a0a9e16df5dbd93330d48
TheKingsident <kingsident@gmail.com>
2025-05-29 20:52:59 +0100

    Add unit tests for get_json function with parameterization

--- 0x03-Unittests_and_integration_tests/test_utils_new.py
+++ 0x03-Unittests_and_integration_tests/test_utils_new.py
@@ -4,6 +4,7 @@
 
 from utils import access_nested_map, get_json, memoize
 from unittest import TestCase, main
+from unittest.mock import patch, MagicMock
 from parameterized import parameterized
 
 
@@ -29,6 +30,22 @@ class TestAccessNestedMap(TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+class TestGetJson(TestCase):
+    """ TestGetJson class
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        mock_response = MagicMock()
+        mock_response.json.return_value = test_payload
+        with patch('requests.get', return_value=mock_response) as mock_get:
+            result = get_json(test_url)
+
+            self.assertEqual(result, test_payload)
+            mock_get.assert_called_once_with(test_url)
 
 
 if __name__ == "__main__":
