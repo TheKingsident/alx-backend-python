f61bdea1c076e2150a29e15c85d98e84da891782
TheKingsident <kingsident@gmail.com>
2025-05-31 12:04:00 +0100

    Refactor ConversationSerializer to include last_message field and update UserSerializer to add full_name field

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,19 +2,11 @@ from rest_framework import serializers
 from .models import User, Conversation, Message
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number']
+        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 'full_name', 'phone_number']
         read_only_fields = ['user_id']
-
-class ConversationSerializer(serializers.ModelSerializer):
-    participants = UserSerializer(many=True, read_only=True)
-    messages = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    
-    class Meta:
-        model = Conversation
-        fields = ['conversation_id', 'participants', 'created_at', 'messages']
-        read_only_fields = ['conversation_id', 'created_at']
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True, source='sender_id')
@@ -24,3 +16,17 @@ class MessageSerializer(serializers.ModelSerializer):
         model = Message
         fields = ['message_id', 'sender', 'recipient', 'message_body', 'sent_at']
         read_only_fields = ['message_id', 'sent_at']
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+    last_message = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'created_at', 'messages', 'last_message']
+        read_only_fields = ['conversation_id', 'created_at']
+
+    def get_last_message(self, obj):
+        last_message = obj.message.last()
+        return last_message.message_body[:30] + "..." if last_message else "No messages yet"
