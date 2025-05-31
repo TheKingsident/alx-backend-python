2b9cf54666f3feb403423dbb9539f8b5db5a882f
TheKingsident <kingsident@gmail.com>
2025-05-31 11:37:29 +0100

    Add 'chats' app to INSTALLED_APPS and set custom user model

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'chats',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ],
 }
+
+# Custom user model
+AUTH_USER_MODEL = 'chats.User'
