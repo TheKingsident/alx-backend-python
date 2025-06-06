dc7506ca8b627aeda90bbe24fd9d7404dcb17db2
TheKingsident <kingsident@gmail.com>
2025-06-06 23:08:48 +0100

    Update pagination settings to increase default page size to 20

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,17 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class CustomPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        # Accessing page.paginator.count for total count
+        total_count = self.page.paginator.count
+        return Response({
+            'count': total_count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data,
+        })
