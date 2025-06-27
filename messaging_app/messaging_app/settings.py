2417363742e8a755b2b2d901d19c16738415610a
TheKingsident <kingsident@gmail.com>
2025-06-27 15:30:16 +0100

    Refactor Dockerfile, update deployment image, and enhance kubectl script for messaging app

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tws$&u($t2jn9!#qfg03ejvrg8efj%d66eo6c5ofzw1+o(cc88
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['messaging.local', 'localhost', '127.0.0.1']
 
 
 import os
